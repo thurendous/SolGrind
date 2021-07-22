4ba2cd89cd8768de7b1417bbc2cc7bc56dc46ba3
thurendous <thurendous2@gmail.com>
2021-07-22 17:36:51 +0900

    first fix

--- blockchain_30_day_lesson/PiggyBank.sol
+++ blockchain_30_day_lesson/PiggyBank.sol
@@ -2,21 +2,21 @@
 pragma solidity >=4.0.22 <0.8.0;
 
 contract PiggyBank {
-    uint public goal;
-    constructor (uint _goal) {
+    uint256 public goal;
+
+    constructor(uint256 _goal) {
         goal = _goal;
     }
 
     receive() external payalbe {} // for receiving ether from EOA
-    
-    function getMyBalance() public view returns(uint) {
+
+    function getMyBalance() public view returns (uint256) {
         return address(this).balace; // return the balance of the contract
     }
 
     function withdraw() public {
-        if(getMyBalance() >= goal) {
-            selfdestruct(msg.sender) // suicide and send ether to the user who calls
+        if (getMyBalance() >= goal) {
+            selfdestruct(msg.sender); // suicide and send ether to the user who calls
         }
     }
-
 }
