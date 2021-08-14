570da2ba7acdfd5eaedc7bf69117eb1ca91ea2cd
thurendous <thurendous2@gmail.com>
2021-08-15 02:18:25 +0900

    change minor

--- blockacademy/7th.sol
+++ blockacademy/7th.sol
@@ -27,17 +27,17 @@ contract Bank {
         require(
             getBalance(msg.sender) >= _amount,
             "no enough balance to withdraw"
-        );
+        ); // Require being called is normal. and it will return the gas remained
         uint256 beforeWithdraw = balance[msg.sender];
         balance[msg.sender] -= _amount + 1;
-        payable(msg.sender).transfer(_amount);
+        payable(msg.sender).transfer(_amount); // "transfer" is not recommended nowadays. "call" function is recommended for some reasons.
         uint256 afterWithdraw = balance[msg.sender];
-        assert(afterWithdraw == beforeWithdraw - _amount);
+        assert(afterWithdraw == beforeWithdraw - _amount); // Assert being called is abnormal. assert will not give back the gas you paid.
     }
 
     function transfer(address _to, uint256 _amount) public {
-        require(getBalance(msg.sender) >= _amount, "no enough balance to send");
-        require(msg.sender != _to, "invalid recipient");
+        require(getBalance(msg.sender) >= _amount, "no enough balance to send"); // check if the amount is available for him to send.
+        require(msg.sender != _to, "invalid recipient"); // check not himself
         balance[msg.sender] -= _amount;
         balance[_to] += _amount;
     }
