d2d4fdaf328454ff492bbf0651c0d9e2379c1ce3
thurendous <thurendous2@gmail.com>
2021-08-15 13:40:48 +0900

    daily grind

--- blockacademy/7th.sol
+++ blockacademy/7th.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
 contract SimpleBank {
@@ -14,8 +15,23 @@ contract SimpleBank {
 
 contract Bank {
     mapping(address => uint256) balance;
+    address owner;
 
-    function deposit() public payable {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "owner can do this.");
+        _;
+    }
+
+    modifier balanceCheck(uint256 _amount) {
+        require(balance[msg.sender] >= _amount, "insufficient amount");
+        _;
+    }
+
+    function deposit() public payable onlyOwner {
         balance[msg.sender] += msg.value;
     }
 
@@ -23,11 +39,7 @@ contract Bank {
         return balance[_addr];
     }
 
-    function withdraw(uint256 _amount) public {
-        require(
-            getBalance(msg.sender) >= _amount,
-            "no enough balance to withdraw"
-        ); // Require being called is normal. and it will return the gas remained
+    function withdraw(uint256 _amount) public balanceCheck(_amount) {
         uint256 beforeWithdraw = balance[msg.sender];
         balance[msg.sender] -= _amount + 1;
         payable(msg.sender).transfer(_amount); // "transfer" is not recommended nowadays. "call" function is recommended for some reasons.
@@ -35,8 +47,11 @@ contract Bank {
         assert(afterWithdraw == beforeWithdraw - _amount); // Assert being called is abnormal. assert will not give back the gas you paid.
     }
 
-    function transfer(address _to, uint256 _amount) public {
-        require(getBalance(msg.sender) >= _amount, "no enough balance to send"); // check if the amount is available for him to send.
+    function transfer(address _to, uint256 _amount)
+        public
+        onlyOwner
+        balanceCheck(_amount)
+    {
         require(msg.sender != _to, "invalid recipient"); // check not himself
         balance[msg.sender] -= _amount;
         balance[_to] += _amount;
